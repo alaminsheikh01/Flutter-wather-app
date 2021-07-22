c2ceb96100286f064e5c4b5c15882084c12a09ae
alamin <alamin@gmail.com>
2021-07-22 16:21:44 +0600

    Exception handling

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -21,6 +21,24 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    String myMargin =
+        'abc'; // I have parese this value as a double so here need to int value as a string. Otherwise this is false value.
+    double myMarginAsAdobule;
+
+    try {
+      myMarginAsAdobule = double.parse(myMargin);
+    } catch (e) {
+      // if try block is false then catch block will run.
+      print(e);
+      //myMarginAsAdobule = 20.0;
+    }
+
+    return Scaffold(
+      body: Container(
+        // if myMarginAsadouble is not true then 100 will run as a margin.
+        margin: EdgeInsets.all(myMarginAsAdobule ?? 100.0),
+        color: Colors.red,
+      ),
+    );
   }
 }
